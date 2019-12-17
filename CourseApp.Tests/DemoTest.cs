84f5adb9769bfca3a94c851a4dc431e92a2f963d
He11am <atlascloud1301@gmail.com>
2019-12-17 19:07:32 +0300

    мат функции без тестов

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -1,28 +1,27 @@
-//Еще не доделано
+// Еще не доделано
+
 using System;
 using Xunit;
 
 namespace CourseApp.Tests
 {
-    public class DemoTest
+    /* public class DemoTest
     {
         [Theory]                      x
         [InlineData(1.1, 0.009, 2.8, 20.109)] //tst1
         [InlineData(4.1, 2.7, 3.6, 13.568)] //tst2
         public void TestEquat(double a, double b, double x, double exp)
         {
-            Assert.Equal(Program.Equat(a, b, x), exp, 3); 
+            Assert.Equal(Program.Equat(a, b, x), exp, 3);
         }
-        
+
         [Fact]
         public void TestTaskA()
         {
             var res = Program.TaskA(1.1, 0.09, 1.2, 2.2, 0.2);
             Assert.Equal(res, new double[0]);
         }
-        
-        
-        
+
         [Fact]
         public void Test1()
         {
@@ -38,5 +37,5 @@ namespace CourseApp.Tests
             var res = Program.MyFunction(a, b, x);
             Assert.Equal(exp, res, 3);
         }
-    }
+    }*/
 }
